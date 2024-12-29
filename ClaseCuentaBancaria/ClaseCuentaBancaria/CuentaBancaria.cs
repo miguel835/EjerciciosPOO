577fed0912a391231ac9063c4235ea9d5fc2f40a
miguel <roberthzambrano235@gmail.com>
2024-12-28 20:23:20 -0500

    Ejercicio clase CuentaBancaria

---
+++ ClaseCuentaBancaria/ClaseCuentaBancaria/CuentaBancaria.cs
@@ -0,0 +1,92 @@
+﻿/* Clase Cuenta Bancaria
+- Crear una clase CuentaBancaria que represente una cuenta bancaria
+- La clase debe tener atributos para el numero de cuenta, el titular, el saldo y el tipo de cuenta (ahorro, corriente)
+- Implementar un constructor que inicialice los atributos
+- Implementar metodos para depositar dinero (depositar(double monto)), retirar dinero (retirar(double monto)), 
+consultar saldo (consultarSaldo()) y obtener el tipo de cuenta (getTipoCuenta()). 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ClaseCuentaBancaria
+{
+    public class CuentaBancaria
+    {
+        //Atributos
+        public string NumeroCuenta;
+        public string Titular;
+        public double Saldo;
+        public string TipoCuenta;
+        //Constructor
+        public CuentaBancaria(string numCuenta, string nombreDelTitular, double CantidadDeDinero, string TypeCuenta)
+        {
+            this.NumeroCuenta = numCuenta;
+            this.Titular = nombreDelTitular;
+            this.Saldo = CantidadDeDinero;
+            this.TipoCuenta = TypeCuenta;
+        }
+        //Metodos
+        public void depositar()
+        {
+            Console.Write("Ingrese el monto a depositar: ");
+            float monto = float.Parse(Console.ReadLine());
+            if (monto <= 0)
+            {
+                string cadenaMonto = monto.ToString();
+                if (cadenaMonto.Contains("-"))
+                {
+                    cadenaMonto = cadenaMonto.Remove('-');
+                    monto = float.Parse(cadenaMonto);
+                    Saldo += monto;
+                    Console.WriteLine("Deposito realizado exitosamente");
+                }
+                else if (monto == 0) Console.WriteLine("No aceptamos ceros");
+            }
+            Saldo += monto;
+            Console.WriteLine("Deposito realizado exitosamente");
+        }
+        public void retirar()
+        {
+            Console.Write("Ingrese el monto a retirar: ");
+            float montoRetirar = float.Parse(Console.ReadLine());
+
+            if (montoRetirar <= 0)
+            {
+                string cadenaMontoRetiro = montoRetirar.ToString();
+                if (cadenaMontoRetiro.Contains("-"))
+                {
+                    cadenaMontoRetiro = cadenaMontoRetiro.Remove('-');
+                    montoRetirar = float.Parse(cadenaMontoRetiro);
+                    Saldo -= montoRetirar;
+                    Console.WriteLine("Retiro realizado exitosamente");
+                }
+                else if (montoRetirar == 0) Console.WriteLine("No aceptamos ceros");
+            }
+            else if (montoRetirar > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+            else if (montoRetirar < Saldo)
+            {
+                Saldo -= montoRetirar;
+                Console.WriteLine("Retiro realizado exitosamente");
+            }
+            
+        }
+        public string consultarSaldo() 
+        {
+            //Console.WriteLine($"El saldo actual de la cuenta es: {Saldo}");
+            return Saldo.ToString();
+        }
+        public string getTipoCuenta()
+        {
+            //Console.WriteLine($"El tipo de cuenta es: {TipoCuenta}");
+            return TipoCuenta.ToString();
+        }
+    }
+}
---
+++ ClaseCuentaBancaria/ClaseCuentaBancaria/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClaseCuentaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Configuración Visual de la consola
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.SetCursorPosition(10, 0);
+            Console.WriteLine("CLASE CUENTA BANCARIA");
+            Console.WriteLine("BIENVENIDO AL BANCO 3 TELEMATICA");
+            Console.Write("Ingrese el nombre del titular de la cuenta: ");
+            string Titular = Console.ReadLine();
+            Console.Write("Ingrese el tipo de cuenta [ahorro/corriente]: ");
+            string tipo = Console.ReadLine();
+            Console.Write("Ingrese la cantidad de dinero que va a dejar en la cuenta: ");
+            double valorDinero = double.Parse(Console.ReadLine());
+            Random aleatorio = new Random();
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(aleatorio.Next(100, 10000000).ToString(), Titular, valorDinero, tipo);
+            Console.WriteLine("Listo ahora ya tiene su cuenta y puede realizar depositos y retiros");
+            Thread.Sleep(500);
+            int opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Menu de opciones");
+                Console.WriteLine("[0] Depositar");
+                Console.WriteLine("[1] Retirar");
+                Console.WriteLine("[2] Ver el tipo de cuenta");
+                Console.WriteLine("[3] Consultar saldo");
+                Console.WriteLine("[4] Salir");
+                Console.Write("Ingresar opción: ");
+                while (!int.TryParse(Console.ReadLine(), out opcion) || opcion < 0 || opcion > 4)
+                {
+                    Console.WriteLine("Opción inválida. Intente nuevamente.");
+                }
+                switch (opcion)
+                {
+                    case 0: 
+                        cuentaBancaria.depositar();
+                        break;
+                    case 1:
+                        cuentaBancaria.retirar();
+                        break;
+                    case 2:
+                        Console.WriteLine($"El tipo de cuenta es: {cuentaBancaria.getTipoCuenta()}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"El saldo de la cuenta es : {cuentaBancaria.consultarSaldo()}");
+                        break;
+                    case 4:
+                        Console.WriteLine("Ha salido exitosamente");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
